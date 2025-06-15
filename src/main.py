ecabe09b0608b54a8aa331390523887c9afbb8f9
Hans Buss <hans.buss@mailfence.com>
2025-06-15 15:34:58 -0300

    Everything else

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import parse 
+import mst 
+
+def main():
+    def_file_path = "ispd18_sample/ispd18_sample.input.def"
+    design = parse.parse_design(def_file_path)
+    
+    G_mst = mst.build_mst_per_net(design)
+    
+    print(f"Design contains {len(design['components'])} components")
+    print(f"Design contains {len(design['nets'])} nets")
+    print(f"MST contains {len(G_mst.edges())} connections")
+    
+    mst.draw_manhattan_mst(G_mst)
+
+if __name__ == "__main__":
+    main()
+
